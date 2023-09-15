1f7e6fcdf9fbe8baf95205c3e720eed7d9124d3f
red2 <pankaj.pmuchhadiya@gmail.com>
2023-09-15 08:06:44 +0530

    Listview, builder, separated, random class

--- lib/util.dart
+++ lib/util.dart
@@ -35,4 +35,28 @@ List<Map> quotesList = [
     "author": "Admin",
     "colors": ["D948B0", "A631D9"],
   },
+  {
+    "quotes":
+    "Kabhi udaas ho jayo to btana mujhe fir se apna dil denge tumhe khelne k liye",
+    "author": "meet",
+    "colors": ["61903B", "907C23"],
+  },
+  {
+    "quotes":
+    "Tera Saath hai Toh mujhe kya kami hai Teri har muskan se mili mujhe kushi hai Muskuraate Rehna isi Tarah humesha Kyonki teri is muskan mein meri jaan basi hai",
+    "author": "Dhyey",
+    "colors": ["900200", "904934"],
+  },
+  {
+    "quotes":
+    "kon kaheta hai taj mahel banane ke lie dolat nahi milti. taj mahel banane ke lie dolat to milti he magar mohbatt karne ke lie mumtaj nahi milti.",
+    "author": "Kavi",
+    "colors": ["8C908E", "767490"],
+  },
+  {
+    "quotes":
+    "Na Ruthna Humse Hum Mar Jayenge, Dil Ki Duniya Tabaah Kr Jayenge, Pyar Kiya He Humne Koi Mazak Nahi Dil Ki Dhadkan Tere Naam Kr Jayenge",
+    "author": "Admin",
+    "colors": ["D948B0", "A631D9"],
+  },
 ];
--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:quotes_app/model/quotes_model.dart';
 import 'package:quotes_app/util.dart';
@@ -17,37 +19,111 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text("Quotes"),
         centerTitle: true,
       ),
-      body: Column(
-        children: quotesList.map((e) {
-          QuotesModel quotesModel = QuotesModel.fromMap(e);
+      // body: Column(
+      //   children: quotesList.map((e) {
+      //     QuotesModel quotesModel = QuotesModel.fromMap(e);
+      //
+      //     List<Color> color = quotesModel.colors?.map((e) {
+      //           int? c = int.tryParse("0xff$e");
+      //           return Color(c ?? 0xffffffff);
+      //         }).toList() ??
+      //         [];
+      //
+      //     return InkWell(
+      //       onTap: () {
+      //         showMyDialog(quotesModel);
+      //       },
+      //       child: Container(
+      //         margin: EdgeInsets.all(10),
+      //         padding: EdgeInsets.all(10),
+      //         decoration: BoxDecoration(gradient: LinearGradient(colors: color)),
+      //         child: Text(quotesModel.quotes ?? ""),
+      //       ),
+      //     );
+      //   }).toList(),
+      // ),
 
-          List<Color> color = quotesModel.colors?.map((e) {
-                int? c = int.tryParse("0xff$e");
-                return Color(c ?? 0xffffffff);
-              }).toList() ??
-              [];
-          return InkWell(
-            onTap: () {
-              showDialog(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(
-                    title: Text("Quotes"),
-                    backgroundColor: color.first,
-                    content: Text(quotesModel.quotes ?? ""),
-                  );
-                },
-              );
-            },
-            child: Container(
-              margin: EdgeInsets.all(10),
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(gradient: LinearGradient(colors: color)),
-              child: Text(quotesModel.quotes ?? ""),
-            ),
-          );
-        }).toList(),
+      /// simple List View
+      // body: ListView(
+      //   padding: EdgeInsets.symmetric(vertical: 20),
+      //   // scrollDirection: Axis.vertical,
+      //   // reverse: true,
+      //   // physics: BouncingScrollPhysics(),
+      //   children: [
+      //     //.... List of Widget
+      //   ],
+      // ),
+
+      /// ListView builder name constructor
+      // body: ListView.builder(
+      //   itemBuilder: (context, index) {
+      //     QuotesModel quotesModel = QuotesModel.fromMap(quotesList[index]);
+      //     List<Color> color = quotesModel.colors?.map((e) {
+      //       int? c = int.tryParse("0xff$e");
+      //       return Color(c ?? 0xffffffff);
+      //     }).toList() ??
+      //         [];
+      //
+      //     return InkWell(
+      //       onTap: () {
+      //         showMyDialog(quotesModel);
+      //       },
+      //       child: Container(
+      //         margin: EdgeInsets.all(10),
+      //         padding: EdgeInsets.all(10),
+      //         decoration: BoxDecoration(gradient: LinearGradient(colors: color)),
+      //         child: Text(quotesModel.quotes ?? ""),
+      //       ),
+      //     );
+      //   },
+      //   itemCount: quotesList.length,
+      // ),
+
+      /// ListView separated name constructor
+      body: ListView.separated(
+          itemBuilder: (context, index) {
+            Map qt = quotesList[index];
+            var quotesModel = QuotesModel.fromMap(qt);
+
+            return Text(quotesModel.quotes ?? "");
+          },
+          separatorBuilder: (context, index) {
+            return Divider(
+              thickness: 5,
+            );
+          },
+          itemCount: quotesList.length),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          var nextInt = Random().nextInt(6);
+          Map qt = quotesList[nextInt];
+          QuotesModel quotesModel = QuotesModel.fromMap(qt);
+
+          showMyDialog(quotesModel);
+          print(nextInt);
+        },
+        child: Icon(Icons.refresh),
       ),
+    );
+  }
+
+  void showMyDialog(QuotesModel quotesModel) {
+    List<Color> color = quotesModel.colors?.map((e) {
+          int? c = int.tryParse("0xff$e");
+          return Color(c ?? 0xffffffff);
+        }).toList() ??
+        [];
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Quotes"),
+          backgroundColor: color.first,
+          content: Text(quotesModel.quotes ?? ""),
+        );
+      },
     );
   }
 }
