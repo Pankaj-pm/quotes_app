1bc90f9f163b5a9c738f6d012c5928c493854ab4
red2 <pankaj.pmuchhadiya@gmail.com>
2023-09-21 08:08:36 +0530

    set background img, dynamic text, color , font size

--- lib/views/quotes_detail.dart
+++ lib/views/quotes_detail.dart
@@ -29,15 +29,41 @@ class _QuotesDetailState extends State<QuotesDetail> {
     GoogleFonts.moul(),
   ];
 
-  int cIndex=0;
-  int fIndex=0;
+  List<String> img = [
+    "assets/Daco_4599778.png",
+    "assets/i1.jpg",
+    "assets/pngwing.com.png",
+    "assets/red-wall-with-white-spray-background.jpg",
+    "assets/pexels.jpg"
+  ];
+
+  int cIndex = 0;
+  int fIndex = 0;
+  int? iIndex;
+  String text = "";
+  double textSize = 12;
+  bool isBold = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("QuotesDetail"),
+        actions: [
+          IconButton(
+              onPressed: () {
+                cIndex = 0;
+                fIndex = 0;
+                iIndex = null;
+                text = "";
+                textSize = 12;
+                isBold = false;
+                setState(() {});
+              },
+              icon: Icon(Icons.refresh)),
+        ],
       ),
+      resizeToAvoidBottomInset: false,
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
@@ -45,13 +71,68 @@ class _QuotesDetailState extends State<QuotesDetail> {
             flex: 4,
             child: Container(
               color: Colors.black12,
-              child: Center(
-                  child: Text(
-                "Quotes",
-                // style: fonts[fIndex].copyWith(color: colors[cIndex],fontSize: 35),
-                style: TextStyle(fontFamily: "Schyler"),
-              )),
+              child: Stack(
+                children: [
+                  if (iIndex != null)
+                    Positioned.fill(
+                      child: Image.asset(img[iIndex!], fit: BoxFit.cover),
+                    ),
+                  Center(
+                    child: Text(
+                      text,
+                      style: fonts[fIndex].copyWith(
+                          color: colors[cIndex],
+                          fontSize: textSize,
+                          fontWeight: isBold ? FontWeight.bold : FontWeight.normal),
+                      // style: TextStyle(fontFamily: "Schyler",fontSize: textSize,fontWeight: isBold ? FontWeight.bold:FontWeight.normal),
+                    ),
+                  ),
+                ],
+              ),
             ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              hintText: "Enter Text",
+              fillColor: Colors.grey,
+              filled: true,
+              // border: OutlineInputBorder.none
+            ),
+            onChanged: (value) {
+              text = value;
+              setState(() {});
+            },
+          ),
+          Row(
+            children: [
+              IconButton(
+                  onPressed: () {
+                    textSize++;
+                    setState(() {});
+                  },
+                  icon: Icon(
+                    Icons.format_size,
+                    size: 28,
+                  )),
+              IconButton(
+                  onPressed: () {
+                    textSize--;
+                    setState(() {});
+                  },
+                  icon: Icon(
+                    Icons.format_size,
+                    size: 20,
+                  )),
+              IconButton(
+                  onPressed: () {
+                    isBold = !isBold;
+                    setState(() {});
+                  },
+                  icon: Icon(
+                    Icons.format_bold,
+                    size: 20,
+                  )),
+            ],
           ),
           Container(
             height: 100,
@@ -61,11 +142,9 @@ class _QuotesDetailState extends State<QuotesDetail> {
               scrollDirection: Axis.horizontal,
               itemBuilder: (context, index) {
                 return InkWell(
-                  onTap: (){
-                    fIndex=index;
-                    setState(() {
-
-                    });
+                  onTap: () {
+                    fIndex = index;
+                    setState(() {});
                   },
                   child: Container(
                       padding: EdgeInsets.all(10),
@@ -89,15 +168,37 @@ class _QuotesDetailState extends State<QuotesDetail> {
               itemBuilder: (context, index) {
                 return InkWell(
                   onTap: () {
-                    cIndex=index;
-                    setState(() {
-                    });
+                    cIndex = index;
+                    setState(() {});
                   },
                   child: Container(
                     height: 40,
                     width: 40,
                     color: colors[index],
                     margin: EdgeInsets.all(10),
+                  ),
+                );
+              },
+            ),
+          ),
+          Container(
+            height: 60,
+            margin: EdgeInsets.all(10),
+            color: Colors.black12,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: img.length,
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () {
+                    iIndex = index;
+                    setState(() {});
+                  },
+                  child: Container(
+                    height: 40,
+                    width: 40,
+                    margin: EdgeInsets.all(10),
+                    child: Image.asset(img[index]),
                   ),
                 );
               },
